aec792e7e264102529b63c8ee574936a8dcb9d4b
jenryEduardo <eduardogomezcalvo851@gmail.com>
2024-11-21 20:24:06 -0600

    fixed: merge fusion

--- src/routes/inventory.routes.js
+++ src/routes/inventory.routes.js
@@ -1,10 +1,13 @@
+const express = require('express');
+const inventoryController = require('../controllers/inventarioController');
+const router = express.Router();
+const {getToken} = require('../middleware/token.js')
+router.post('/add-inventory',getToken, inventoryController.addInventory);
+router.get('/view/:nombreFormula',getToken, inventoryController.getProduct);
+router.put('/actualizar/:id',getToken, inventoryController.actualizar);
+router.delete('/eliminar/:id',getToken,inventoryController.deleteProduct)
+router.put('/add/:nombre',getToken,inventoryController.addPresentations)
+router.get('/view-all',inventoryController.nameProduct)
+//updatequantityMedicament
+module.exports = router;
 
-const express=require('express')
-const inventoryController=require('../controllers/inventarioController.js')
-const router=express.Router()
-
-router.post('/add-inventory',inventoryController.addInventory)
-router.get('/view',inventoryController.getProduct)
-router.put('/actualizar/:id',inventoryController.actualizar)
-router.delete('/eliminar/:id',inventoryController.deleteProduct)
-module.exports=router
